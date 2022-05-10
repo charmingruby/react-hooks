96d7d62555945bfb1154cff559766f2a236d0a05
charmingruby <mrrubycanal3105@gmail.com>
2022-05-10 14:15:33 -0300

    state finalizado

--- react-hooks/src/views/examples/UseEffect.jsx
+++ react-hooks/src/views/examples/UseEffect.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import { useState } from 'react'
 
 const UseEffect = (props) => {
+
+    const [number, setNumber] = useState(1)
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <div className='center'>
+                <div>
+                    <span className='text'>Fatorial: </span>
+                    <span className='text red'>100</span>
+                </div>
+                <input type="number" className='input'
+                    value={number}
+                    onChange={e => setNumber(e.target.value)} />
+            </div>
         </div>
     )
 }
